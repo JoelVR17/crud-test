9ca03f0cca70e7f2218d3e3a988a25001dd8b0e3
Joel Vargas <vermudo.com@gmail.com>
2025-09-05 16:39:57 -0600

    fix: remove empty password field in user registration

--- src/app/api/users/register/route.ts
+++ src/app/api/users/register/route.ts
@@ -23,7 +23,7 @@ export async function POST(request: Request) {
     const user = await prisma.user.upsert({
       where: { id },
       update: { email },
-      create: { id, email, password: "" },
+      create: { id, email },
     });
 
     return NextResponse.json({
